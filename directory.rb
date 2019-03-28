7b5b66dadbb176c272b71b93230cb5c889b17c3b
shektor <44544977+shektor@users.noreply.github.com>
2019-03-28 22:58:48 +0000

    Refactor menu method
    The interactive_method() mennu was too long. Its contents were extracted
    into 2 methods that will show the options and then take the selection as
    an arguement, to be processed by the process() method.

--- directory.rb
+++ directory.rb
@@ -41,26 +41,21 @@ end
 
 def input_students
   
-  students = []
-
   puts "Enter a student name and hit enter"
   puts "Names can be added until you hit enter on a blank line"
   name = gets.chomp
   
   while !name.empty? do
     
-    students << {name: name, cohort: :november}
+    @students << {name: name, cohort: :november}
     
-    puts msg_end_plural("Now we have #{students.count} student", students.count)
+    puts msg_end_plural("Now we have #{@students.count} student", @students.count)
 
     name = gets.chomp
     
     # chomp can be replaced with the delete method if correct arguements are passed
     # name = gets.delete("\r\n")
   end
-  
-  # return user
-  students
   
 end
 
@@ -70,40 +65,38 @@ def print_menu
   puts "9. Exit"
 end
 
-def interactive_menu
-  
-  students = []
-  
-  loop do
-  
-    # 1. print the menu and ask the user what to do
-    print_menu
-    
-    # 2. read the input and save it into a variable
-    selection = gets.chomp
-    
-    # 3. do what the user has asked
-    case selection
-      when "1"
-        students = input_students
-      when "2"
-        if students.count > 0
-          print_header
-          print_with_index(students)
-          print_footer(students)
-        else
-          puts "The directory is empty"
-        end
-      when "9"
-        exit
+def show_students
+  print header
+  print(students)
+  print_footer(students)
+end
+
+def process(selection)
+  case selection
+    when "1"
+      input_students
+    when "2"
+      if @students.count > 0
+        print_header
+        print_with_index(@students)
+        print_footer(@students)
       else
-        puts "Invalid Input, please try again"
-    end
-  
-  # 4. repeat from step 1
+        puts "The directory is empty"
+      end
+    when "9"
+      exit
+    else
+      puts "Invalid Input, please try again"
   end
 end
 
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
 
+@students = []
 interactive_menu
 
