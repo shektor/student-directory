6bcd60d25f15ff7592e1e5fce53d631fb27cde77
shektor <44544977+shektor@users.noreply.github.com>
2019-03-31 12:11:50 +0000

    Add method to update students from file

--- directory.rb
+++ directory.rb
@@ -72,6 +72,15 @@ def save_students
   file.close
 end
 
+def load_students
+  file - File.open("students.csv", "r")
+  file.readlines.each do |line|
+    name, cohort = line.chomp.split(",")
+    @students << {name: name, cohort: cohort.to_sym}
+  end
+  file.close
+end
+
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
