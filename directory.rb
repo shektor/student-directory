255c23cc1d470c7b0241248a577b9ebb28643e93
shektor <44544977+shektor@users.noreply.github.com>
2019-03-31 12:15:14 +0000

    Fix file assignment error

--- directory.rb
+++ directory.rb
@@ -73,7 +73,7 @@ def save_students
 end
 
 def load_students
-  file - File.open("students.csv", "r")
+  file = File.open("students.csv", "r")
   file.readlines.each do |line|
     name, cohort = line.chomp.split(",")
     @students << {name: name, cohort: cohort.to_sym}
